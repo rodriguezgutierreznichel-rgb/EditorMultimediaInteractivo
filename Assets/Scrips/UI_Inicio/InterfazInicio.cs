29bc33e1f09ea6d286f7d5942ec74bf5092b92c3
rodriguezgutierreznichel-rgb <rodriguezgutierreznichel@gmail.com>
2025-11-06 17:48:22 +0000

    Fase 4
    Se realizo los ajustes para la interfaz de inicio y el de pausa, la primera vez va bien, pero cuando vuelves a empezar el juego no sale el botón de pausar.

--- Assets/Scrips/UI_Inicio/InterfazInicio.cs
+++ Assets/Scrips/UI_Inicio/InterfazInicio.cs
@@ -1,13 +1,30 @@
 using UnityEngine;
-
+using TMPro;
 public class InterfazInicio : MonoBehaviour
 {
-    GameObject interfazDeInicio;
+    [SerializeField]
+    TextMeshProUGUI tituloDelJuego;
+
+    [SerializeField]
+    TextMeshProUGUI nombreDelCreador;
+
+    [SerializeField] GameObject botonJugar;
+
+    public GameObject interfazDeInicio;
+
+    public float tiempoDeAnimacion = 0;
+    public LeanTweenType tipoDeCurvaDelTituloDelJuego;
+    public LeanTweenType tipoDeCurvaDelNombreDelCreador;
+    public LeanTweenType tipoDeCurvaDelBoton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         interfazDeInicio.SetActive(true);
+        AnimacionDelTituloDelJuego();
+        AnimacionDelNombreDelCreador();
+        AnimacionDelBotonDeEmpezarJuego();
+       
     }
 
     // Update is called once per frame
@@ -19,5 +36,19 @@ public class InterfazInicio : MonoBehaviour
     public void EmpezarJuego()
     {
         interfazDeInicio.SetActive(false);
+
+    }
+
+    void AnimacionDelTituloDelJuego()
+    {
+        LeanTween.scale(tituloDelJuego.rectTransform, new Vector3(3f, 3f, 1f), tiempoDeAnimacion).setEase(tipoDeCurvaDelTituloDelJuego);
+    }
+    void AnimacionDelNombreDelCreador()
+    {
+        LeanTween.scale(nombreDelCreador.rectTransform, new Vector3(1.5f, 1.5f, 1f), tiempoDeAnimacion).setEase(tipoDeCurvaDelNombreDelCreador);
+    }
+    void AnimacionDelBotonDeEmpezarJuego()
+    {
+        LeanTween.scale(botonJugar, new Vector3(2f, 2f, 1f), tiempoDeAnimacion).setEase(tipoDeCurvaDelBoton);
     }
 }
