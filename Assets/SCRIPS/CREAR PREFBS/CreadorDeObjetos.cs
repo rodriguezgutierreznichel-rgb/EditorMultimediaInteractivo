04ce5fe7bfc9be489f9f1a9742247699eadd5dac
rodriguezgutierreznichel-rgb <rodriguezgutierreznichel@gmail.com>
2025-11-09 11:34:09 +0000

    Fase 11
    Se ha realizado el scrip "CreadorDeObjeto", al boton "Techo 1" que crear un techo especifico, y permite moverlo (sigue el raton), rotar (con la rueda del raton) y eliminar (con el boton "Retroceso" del teclado). Falta añadir esto al resto de objetos, y aplicar las demas funciones a los otros botones del menu de accion.

---
+++ Assets/SCRIPS/CREAR PREFBS/CreadorDeObjetos.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class CreadorDeObjetos : MonoBehaviour
+{
+    GameObject currentGameObject = null;
+    GameObject prefabSeleccionado;
+    float rotacionY = 0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverObjeto();
+        ColocarObjeto();
+        RotarObjeto();
+        BorrarObjeto();
+    }
+    public void SeleccionarPrefab(GameObject prefab)
+    {
+        prefabSeleccionado = prefab;
+
+        // Crear uno nuevo listo para mover
+        currentGameObject = Instantiate(prefabSeleccionado);
+    }
+
+    void MoverObjeto()
+    {
+        if (currentGameObject == null) return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        currentGameObject.SetActive(false);
+
+        if (Physics.Raycast(ray, out hit, 100f))
+        {
+            currentGameObject.transform.position = hit.point + Vector3.up;
+        }
+
+        currentGameObject.SetActive(true);
+    }
+
+    void ColocarObjeto()
+    {
+        if (currentGameObject != null && Input.GetMouseButtonDown(0))
+        {
+            // Suelta el objeto y permite crear otro más tarde
+            currentGameObject = null;
+        }
+    }
+    void RotarObjeto()
+    {
+        if (currentGameObject == null) return;
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0f)
+        {
+            rotacionY += scroll * 120f; // sensibilidad
+            currentGameObject.transform.rotation = Quaternion.Euler(0, rotacionY, 0);
+        }
+    }
+    void BorrarObjeto()
+    {
+        if (currentGameObject != null && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Destroy(currentGameObject);
+            currentGameObject = null;
+        }
+    }
+}
