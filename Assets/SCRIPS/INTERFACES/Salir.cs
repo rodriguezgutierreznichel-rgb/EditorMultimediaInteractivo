e63930fa50b7e300766526fc3fe2e9b08d8aa131
rodriguezgutierreznichel-rgb <rodriguezgutierreznichel@gmail.com>
2026-02-25 19:39:57 +0000

    algo

---
+++ Assets/SCRIPS/INTERFACES/Salir.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Salir : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+    }
+}
